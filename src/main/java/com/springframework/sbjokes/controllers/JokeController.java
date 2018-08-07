1fc0fe150daf8273023a4f4459cead4f299f86c0
nasiriqbal786 <mehr_nasir@hotmail.com>
2018-08-07 13:09:43 +1000

    Final Commit

---
+++ src/main/java/com/springframework/sbjokes/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package com.springframework.sbjokes.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springframework.sbjokes.services.JokeService;
+
+@Controller
+public class JokeController {
+
+	private JokeService jokeService;
+
+	@Autowired
+	public JokeController(JokeService jokeService) {
+		this.jokeService = jokeService;
+	}
+	
+	@RequestMapping({"/joke"})
+	public String showJoke(Model model) {
+		model.addAttribute("joke", jokeService.getJoke());
+		return "chucknorris";
+	}
+}
---
+++ src/main/java/com/springframework/sbjokes/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.springframework.sbjokes.services;
+
+public interface JokeService {
+
+	String getJoke();
+}
