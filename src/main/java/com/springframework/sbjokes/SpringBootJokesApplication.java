bce1d8859e2f8834c123fb3f73803611548c9c18
nasiriqbal786 <mehr_nasir@hotmail.com>
2018-08-07 10:28:20 +1000

    Initial Create

---
+++ src/main/java/com/springframework/sbjokes/SpringBootJokesApplication.java
@@ -0,0 +1,12 @@
+package com.springframework.sbjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJokesApplication.class, args);
+	}
+}
